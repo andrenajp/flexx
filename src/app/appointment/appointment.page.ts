554f8fcb42a7e2883f2fffd4bfb412729033cacd
andrena solution <andrenajp@gmail.com>
2021-02-08 12:08:38 -0300

    home-profil

--- src/app/appointment/appointment.page.ts
+++ src/app/appointment/appointment.page.ts
@@ -14,14 +14,14 @@ export class AppointmentPage implements OnInit {
     img: '../../assets/images/salon4.png',
     name: 'Martha Salon',
     address: '463 W Broadway, New York, NY 10012, United...',
-    date: '05 Nov, 2020 - 10:00AM',
+    date: '15 Fev, 2021 - 10H00',
     rate: '5.0',
     km: '3.2km'
   }, {
     img: '../../assets/images/salon6.png',
     name: 'Central City Bright',
     address: '463 W Broadway, New York, NY 10012, United...',
-    date: '07 Nov, 2020 - 07:00PM',
+    date: '17 Fev, 2021 - 17:00',
     rate: '5.0',
     km: '3.2km'
   }]
@@ -29,28 +29,28 @@ export class AppointmentPage implements OnInit {
     img: '../../assets/images/salon2.png',
     name: 'Bright & Light',
     address: '463 W Broadway, New York, NY 10012, United...',
-    date: '20 Oct, 2020 - 07:00PM',
+    date: '20 Oct, 2020 - 08H00',
     rate: '5.0',
     km: '3.2km'
   }, {
     img: '../../assets/images/salon9.png',
     name: 'Green Salon',
     address: '463 W Broadway, New York, NY 10012, United...',
-    date: '19 Oct, 2020 - 07:00PM',
+    date: '19 Oct, 2020 - 08H00',
     rate: '5.0',
     km: '3.2km'
   }, {
     img: '../../assets/images/salon6.png',
     name: 'Central City Bright',
     address: '463 W Broadway, New York, NY 10012, United...',
-    date: '17 Oct, 2020 - 07:00PM',
+    date: '17 Oct, 2020 - 17H00',
     rate: '5.0',
     km: '3.2km'
   }, {
     img: '../../assets/images/salon7.png',
     name: 'Beauty Plus',
     address: '463 W Broadway, New York, NY 10012, United...',
-    date: '19 Oct, 2020 - 07:00PM',
+    date: '19 Oct, 2020 - 07H00',
     rate: '5.0',
     km: '3.2km'
   },]
